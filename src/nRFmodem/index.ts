90cd5e498bcf2bf93d401b19bbb3d8d361668914
Bence Frenyó <26139379+bencefr@users.noreply.github.com>
2021-02-09 14:35:24 +0100

    Refactor callback to separate private method

--- src/nRFmodem/index.ts
+++ src/nRFmodem/index.ts
@@ -3,35 +3,39 @@ import SerialPort, { parsers } from 'serialport';
 export type Response = string[];
 
 class ModemPort extends SerialPort {
-    private waitingForResponse: boolean;
+    private waitingForResponse = false;
+
+    private incomingLines: string[] = [];
 
     constructor(path: string, opts = { baudRate: 112500 }) {
         super(path, { ...opts });
 
-        this.waitingForResponse = false;
-
-        const lines: string[] = [];
         this.pipe(new parsers.Readline({ delimiter: '\r\n' })).on(
             'data',
-            (line: string) => {
-                let error;
-                if (line === 'OK') error = null;
-                if (line === 'ERROR') error = 'ERROR';
-                else {
-                    error = line.match(/\+CM[ES] ERROR: (?<cause_value>.*)/)
-                        ?.groups?.cause_value;
-                }
-                if (error !== undefined) {
-                    lines.push(line);
-                    this.emit('response', { lines: lines.slice(0), error });
-                    lines.splice(0);
-                } else if (this.waitingForResponse) {
-                    lines.push(line);
-                } else {
-                    this.emit('line', line);
-                }
-            }
+            this.parseLine.bind(this)
         );
+    }
+
+    private parseLine(line: string) {
+        let error;
+        if (line === 'OK') error = null;
+        if (line === 'ERROR') error = 'ERROR';
+        else {
+            error = line.match(/\+CM[ES] ERROR: (?<cause_value>.*)/)?.groups
+                ?.cause_value;
+        }
+        if (error !== undefined) {
+            this.incomingLines.push(line);
+            this.emit('response', {
+                lines: this.incomingLines.slice(0),
+                error,
+            });
+            this.incomingLines.splice(0);
+        } else if (this.waitingForResponse) {
+            this.incomingLines.push(line);
+        } else {
+            this.emit('line', line);
+        }
     }
 
     writeAT(
