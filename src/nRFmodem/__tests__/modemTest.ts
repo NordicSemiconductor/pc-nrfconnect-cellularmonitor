10f5e53f482011b97b05a10fc543fd5bf5c83c5d
Nicolai Berthelsen <nicolai.berthelsen@nordicsemi.no>
2021-02-17 13:23:55 +0100

    Override jest config from shared and make another attempt at creating first test
    Jest config from shared by default mocks `serialport` module, which is
    currently not helpful, so prevent that config.
    Also rewrite this test to verify that the hardware actually receives the
    intended message.

--- src/nRFmodem/__tests__/modemTest.ts
+++ src/nRFmodem/__tests__/modemTest.ts
@@ -1,37 +1,40 @@
-/* eslint-disable class-methods-use-this */
+import MockBinding from '@serialport/binding-mock';
+import SerialPort, { parsers } from 'serialport';
+
 import ModemPort from '../index';
 
-jest.mock('serialport', () => {
-    class MockSerialPort {
-        emit = jest.fn();
-        write = jest.fn();
-        pipe() {
-            return { on: jest.fn() };
-        }
-        prependOnceListener() {
-            return this;
-        }
-    }
-    return {
-        __esModule: true,
-        default: MockSerialPort,
-        parsers: {
-            Readline: jest.fn().mockImplementation(() => {
-                return {};
-            }),
-        },
-    };
-});
-
 describe('modem', () => {
-    const modem = new ModemPort('some-path');
-    it('should provide callback if it is not provided', () => {
-        modem.writeAT('command');
-        expect(modem.write).toHaveBeenCalledTimes(1);
-    });
+    const [modem, modemReadline] = initialiseModem();
 
-    it('should write command', () => {
-        modem.writeAT('command', jest.fn());
-        expect(modem.write).toHaveBeenCalledWith('command\r\n');
+    it('should write to modem', done => {
+        const inputCommand = 'command';
+
+        modemReadline.on('data', data => {
+            try {
+                expect(data.length).toBe(inputCommand.length);
+                expect(data).toEqual(inputCommand);
+                modem.close(done);
+            } catch (e) {
+                done(e);
+            }
+        });
+        modem.writeAT(inputCommand);
     });
 });
+
+// SETUP
+
+function initialiseModem(): [ModemPort, parsers.Readline] {
+    SerialPort.Binding = MockBinding;
+
+    // Create a port and enable echoing of input
+    MockBinding.createPort('/dev/PORT', {
+        echo: true,
+        readyData: Buffer.from([]),
+    });
+    const modem = new ModemPort('/dev/PORT');
+    const modemReadline = modem.pipe(
+        new parsers.Readline({ delimiter: '\r\n' })
+    );
+    return [modem, modemReadline];
+}
