d9913cc7e251df7199e1c4c7ea563fd0662db3cc
Nicolai Berthelsen <nicolai.berthelsen@nordicsemi.no>
2021-02-19 15:39:12 +0100

    Add a couple more tests for the modem class

--- src/nRFmodem/__tests__/modemTest.ts
+++ src/nRFmodem/__tests__/modemTest.ts
@@ -5,14 +5,13 @@ import ModemPort from '../index';
 const MockBinding = require('@serialport/binding-mock');
 
 describe('modem', () => {
-    const [modem, modemReadline] = initialiseModem();
-
     it('should write to modem', done => {
+        const [modem, modemReadline] = initialiseModem();
+
         const inputCommand = 'command';
 
         modemReadline.on('data', data => {
             try {
-                expect(data.length).toBe(inputCommand.length);
                 expect(data).toEqual(inputCommand);
                 modem.close(done);
             } catch (e) {
@@ -20,6 +19,39 @@ describe('modem', () => {
             }
         });
         modem.writeAT(inputCommand);
+    });
+
+    it('should handle OK response', done => {
+        const [modem] = initialiseModem();
+
+        const okResponse = 'OK';
+        modem.on('response', res => {
+            try {
+                expect(res.lines[0]).toBe(okResponse);
+                expect(res.error).toBe(null);
+                modem.close(done);
+            } catch (e) {
+                done(e);
+            }
+        });
+        modem.writeAT(okResponse);
+    });
+
+    it('should handle errors', done => {
+        const [modem] = initialiseModem();
+        const errorResponse = 'ERROR';
+
+        modem.on('response', res => {
+            try {
+                expect(res.lines[0]).toBe(errorResponse);
+                expect(res.error).toBe(errorResponse);
+                modem.close(done);
+            } catch (e) {
+                done(e);
+            }
+        });
+
+        modem.writeAT(errorResponse);
     });
 });
 
