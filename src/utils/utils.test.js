ab821f9b6cbd079254783caa7a3aced0a2e8cddc
Nicolai Berthelsen <nicolai.berthelsen@nordicsemi.no>
2021-06-11 08:16:33 +0200

    Add test for extracting serialports from device

--- src/utils/utils.test.js
+++ src/utils/utils.test.js
@@ -1,5 +1,6 @@
 import { getNameAndDirectory } from './fileUtils';
 import { truncateMiddle } from './index';
+import { getSerialports } from './serialport';
 
 test('get name and directory', () => {
     expect(getNameAndDirectory('some/path/to/file.bin')).toStrictEqual([
@@ -20,4 +21,20 @@ test('truncate string', () => {
     expect(truncateMiddle('string with 24 chars 123')).toBe(
         'string with 24 chars...3'
     );
+});
+
+test('get device serial ports', () => {
+    const device = {
+        serialport: {
+            path: 'testPath1',
+        },
+        'serialport.1': {
+            path: 'testPath2',
+        },
+        'serialport.2': {
+            path: 'testPath3',
+        },
+    };
+    const ports = getSerialports(device);
+    expect(ports.length).toBe(3);
 });
