72ae1218eddfb5428422306087c2e30f457e9fe5
Marko Schulz <Marko.Schulz@nordicsemi.no>
2022-04-11 15:31:34 +0200

    Break import cycles

--- src/features/wireshark/wiresharkSlice.ts
+++ src/features/wireshark/wiresharkSlice.ts
@@ -6,7 +6,7 @@
 
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-import { RootState } from '../../appReducer';
+import type { RootState } from '../../appReducer';
 import {
     getTsharkPath as getPersistedTsharkPath,
     getWiresharkPath as getPersistedWiresharkPath,
