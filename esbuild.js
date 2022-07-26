85317df4a289cc114541b1192b2d6a84c60cdf95
Pascal Pickel <Pascal.Pickel@nordicsemi.no>
2022-07-26 17:32:39 +0200

    docs: add missing license to files
    Signed-off-by: Pascal Pickel <Pascal.Pickel@nordicsemi.no>

--- esbuild.js
+++ esbuild.js
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2015 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
+ */
+
 const { sassPlugin } = require('esbuild-sass-plugin');
 const esbuild = require('esbuild');
 
--- terminal/webpack.js
+++ terminal/webpack.js
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2015 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
+ */
+
 const webpack = require('webpack');
 const config = require('pc-nrfconnect-shared/config/webpack.config');
 
