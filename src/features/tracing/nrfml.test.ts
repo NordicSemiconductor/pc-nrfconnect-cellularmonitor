7fba46b204a4e62436010e4098b7992325924b54
aadnekar <aadne.karstad@gmail.com>
2023-02-14 13:38:53 +0100

    Change diplay name to Cellular Monitor

--- src/features/tracing/nrfml.test.ts
+++ src/features/tracing/nrfml.test.ts
@@ -219,7 +219,7 @@ describe('nrfml', () => {
             expect(liveConfig).toEqual({
                 name: 'nrfml-wireshark-named-pipe-sink',
                 init_parameters: {
-                    application_name: 'Trace Collector V2 preview',
+                    application_name: 'Cellular Monitor',
                     hw_name: undefined,
                     os_name: 'MockOS',
                     start_process: MOCKED_DEFAULT_WIRESHARK_PATH,
@@ -236,7 +236,7 @@ describe('nrfml', () => {
             expect(pcapConfig).toEqual({
                 name: 'nrfml-pcap-sink',
                 init_parameters: {
-                    application_name: 'Trace Collector V2 preview',
+                    application_name: 'Cellular Monitor',
                     hw_name: undefined,
                     os_name: 'MockOS',
                     file_path: path.join('some', 'path.pcapng'),
