b34fe0186869f8a13e0ae765741a3a057f794d9e
aadnekar <aadne.karstad@gmail.com>
2023-02-22 15:30:19 +0100

    Extend Open Serial Terminal feature
    Most of the feature functionality is dependent on pc-nrfconnect-shared
    and pc-nrfconnect-launcher. This commit implements a button that
    is only enabled if a source is available.

--- src/components/SidePanel/SidePanel.tsx
+++ src/components/SidePanel/SidePanel.tsx
@@ -4,10 +4,20 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
 
-import React from 'react';
-import { SidePanel } from 'pc-nrfconnect-shared';
+import React, { useEffect, useMemo, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { ipcRenderer } from 'electron';
+import {
+    Button,
+    Device,
+    Dropdown,
+    DropdownItem,
+    Group,
+    openAppWindow,
+    selectedDevice,
+    SidePanel,
+} from 'pc-nrfconnect-shared';
 
-import { openAppWindow } from '../../utils/openApp';
 import AdvancedOptions from './AdvancedOptions';
 import EventGraphOptions from './EventGraphOptions';
 import Instructions from './Instructions';
@@ -32,12 +42,111 @@ export const TraceCollectorSidePanel = () => (
         <TraceFileInformation />
         <AdvancedOptions />
         <LoadTraceFile />
+
         <EventGraphOptions />
-        <button
-            type="button"
-            onClick={() => openAppWindow('pc-nrfconnect-programmer')}
-        >
-            Open programmer
-        </button>
+        <OpenSerialTerminal />
     </SidePanel>
 );
+
+const OpenSerialTerminal = () => {
+    const [sources, setSources] = useState<string[]>([]);
+    const [selectedSource, setSelectedSource] = useState<DropdownItem | null>(
+        null
+    );
+    const device = useSelector(selectedDevice);
+
+    useEffect(() => {
+        const getSources = async () => {
+            const localApps = (await ipcRenderer.invoke(
+                'apps:get-local-apps'
+            )) as { name: string; source: string }[];
+            const localSources = localApps
+                .filter(app => app.name === 'pc-nrfconnect-serial-terminal')
+                .map(app => app.source);
+            const { apps: downloadableApps } = (await ipcRenderer.invoke(
+                'apps:get-downloadable-apps'
+            )) as {
+                apps: {
+                    name: string;
+                    source: string;
+                    currentVersion: string;
+                }[];
+            };
+            console.log(downloadableApps);
+            const downloadableSources = downloadableApps
+                .filter(
+                    app =>
+                        app.name === 'pc-nrfconnect-serial-terminal' &&
+                        app.currentVersion != null
+                )
+                .map(app => app.source);
+            setSources([...localSources, ...downloadableSources]);
+        };
+
+        getSources();
+    }, []);
+
+    const sourceItems = useMemo(
+        () =>
+            sources.length > 0
+                ? [
+                      ...sources.map(source => ({
+                          label: source,
+                          value: source,
+                      })),
+                  ]
+                : [{ label: 'Not Installed', value: '' }],
+        [sources]
+    );
+
+    useEffect(() => {
+        setSelectedSource(sourceItems[0]);
+    }, [sourceItems]);
+
+    if (device === undefined) {
+        return null;
+    }
+    return (
+        <Group heading="Open Serial Terminal">
+            <Button
+                large
+                className="btn-secondary w-100"
+                onClick={() =>
+                    openSerialTerminal(
+                        device,
+                        selectedSource?.value ?? 'official'
+                    )
+                }
+                disabled={sources.length === 0}
+            >
+                Open Serial Terminal
+            </Button>
+
+            {sources.length > 1 ? (
+                <Dropdown
+                    selectedItem={selectedSource ?? sourceItems[0]}
+                    items={sourceItems}
+                    onSelect={item => setSelectedSource(item)}
+                />
+            ) : null}
+        </Group>
+    );
+};
+
+const openSerialTerminal = (device: Device, source = 'official') => {
+    // For the moment we assume we only want to open the first available port
+    const vComIndex = 0;
+    const ports = device.serialPorts;
+    const serialPort = ports !== undefined ? ports[vComIndex] : undefined;
+    const serialPortPath = serialPort?.path ?? undefined;
+
+    openAppWindow(
+        { name: 'pc-nrfconnect-serial-terminal', source },
+        {
+            device: {
+                serialNumber: device.serialNumber,
+                serialPortPath,
+            },
+        }
+    );
+};
