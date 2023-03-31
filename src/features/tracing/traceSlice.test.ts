dcd6a027a5a06f48fdbd11a428abf88c062fd101
Marko Schulz <Marko.Schulz@nordicsemi.no>
2023-03-31 12:23:10 +0200

    Use latest shared with esbuild 0.17

--- src/features/tracing/traceSlice.test.ts
+++ src/features/tracing/traceSlice.test.ts
@@ -4,6 +4,8 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
 
+import '../../utils/testUtils';
+
 import { configureStore } from '@reduxjs/toolkit';
 import { testUtils } from 'pc-nrfconnect-shared/test';
 
