875a4bdf22a6184136ba42b5270bcd1bb628f9c6
aadnekar <aadne.karstad@gmail.com>
2023-04-19 09:53:01 +0200

    feat: Add AT commands to generate network report
    In order to make it easier to read and possibly reuse, I've added the
    commands in separate lists, so that we can use them in different
    combinations.

--- src/components/SidePanel/Macros.tsx
+++ src/components/SidePanel/Macros.tsx
@@ -9,133 +9,36 @@ import { useSelector } from 'react-redux';
 import { Button, SerialPort } from 'pc-nrfconnect-shared';
 
 import { getUartSerialPort } from '../../features/tracing/traceSlice';
+import {
+    fullReport,
+    recommendedAt,
+    sendMacros,
+} from '../../features/tracingEvents/at/recommeneded';
 
 export const Macros = () => {
     const serialPort = useSelector(getUartSerialPort);
 
     if (serialPort != null) {
         return (
-            <Button
-                className="w-100"
-                variant="secondary"
-                onClick={() => sendMacros(serialPort, recommendedAt)}
-                title={`Send recommended AT commands over port ${serialPort.path}.\nRemember to Start tracing, in order to update the dashboard and chart.`}
-            >
-                Run recommended AT commands
-            </Button>
+            <>
+                <Button
+                    className="w-100"
+                    variant="secondary"
+                    onClick={() => sendMacros(serialPort, recommendedAt)}
+                    title={`Send recommended AT commands over port ${serialPort.path}.\nRemember to Start tracing, in order to update the dashboard and chart.`}
+                >
+                    Run recommended AT commands
+                </Button>
+                <Button
+                    className="w-100"
+                    variant="secondary"
+                    onClick={() => sendMacros(serialPort, fullReport)}
+                    title={`Send recommended AT commands over port ${serialPort.path}.\nRemember to Start tracing, in order to update the dashboard and chart.`}
+                >
+                    Run Full Network Test
+                </Button>
+            </>
         );
     }
     return null;
-};
-
-const recommendedAt = [
-    'AT+CFUN=1',
-    'AT+CGSN=1',
-    'AT+CGMI',
-    'AT+CGMM',
-    'AT+CGMR',
-    'AT+CEMODE?',
-    'AT%XCBAND=?',
-    'AT+CMEE?',
-    'AT+CNEC?',
-    'AT+CGEREP?',
-    'AT+CIND=1,1,1',
-    'AT+CEREG=5',
-    'AT+CEREG?',
-    'AT+COPS=3,2',
-    'AT+COPS?',
-    'AT%XCBAND',
-    'AT+CGDCONT?',
-    'AT+CGACT?',
-    'AT%CESQ=1',
-    'AT+CESQ',
-    'AT%XSIM=1',
-    'AT%XSIM?',
-    'AT+CPIN?',
-    'AT+CPINR="SIM PIN"',
-    'AT+CIMI',
-    'AT+CNEC=24',
-    'AT+CMEE=1',
-    'AT+CEER',
-    'AT%MDMEV=1',
-    'AT%CESQ=1',
-    'AT+CGEREP=1',
-    'AT%XTIME=1',
-    'AT+CSCON=1',
-    'AT%XPOFWARN=1,30',
-    'AT%XVBATLVL=1',
-    'AT%XMONITOR',
-    'AT%CONEVAL',
-    'AT%XCONNSTAT=1',
-    'AT#XPING="www.google.com",45,5000,5,1000',
-    'AT%XCONNSTAT?',
-    'AT%HWVERSION',
-    'AT%XMODEMUUID',
-    'AT%XDATAPRFL?',
-    'AT%XSYSTEMMODE?',
-];
-
-// Todo: need to set mode in store, in order to not always check for mode
-export const sendMacros = (
-    serialPort: SerialPort,
-    commands: string[],
-    mode?: boolean
-) => {
-    if (mode) {
-        subscribe(serialPort, '', commands);
-    } else {
-        testMode(serialPort, commands);
-    }
-};
-
-const testMode = (serialPort: SerialPort, commands: string[]) => {
-    const decoder = new TextDecoder();
-    let prefix = 'at ';
-    let response = '';
-
-    const testHandler = serialPort.onData(data => {
-        response += decoder.decode(data);
-        const doCompare = response.endsWith('\r\n');
-        if (doCompare) {
-            if (response.includes('ERROR')) {
-                prefix = '';
-            }
-
-            testHandler();
-            subscribe(serialPort, prefix, commands);
-        }
-    });
-
-    serialPort.write(`${prefix} AT\r\n`);
-};
-
-const subscribe = (
-    serialPort: SerialPort,
-    prefix: string,
-    commands: string[]
-) => {
-    const decoder = new TextDecoder();
-    let commandIndex = 0;
-    let response = '';
-
-    const handler = serialPort.onData(data => {
-        response += decoder.decode(data);
-        const doCompare = response.endsWith('\r\n');
-        const doContinue =
-            (doCompare && response.includes('OK')) ||
-            response.includes('ERROR');
-        if (doContinue) {
-            commandIndex += 1;
-
-            if (commandIndex < commands.length) {
-                serialPort.write(`${prefix}${commands[commandIndex]}\r\n`);
-            } else {
-                // Cleanup when all commands have been sent.
-                handler();
-            }
-            response = '';
-        }
-    });
-
-    serialPort.write(`${prefix}${commands[commandIndex]}\r\n`);
 };
--- src/features/tracingEvents/at/recommeneded.ts
+++ src/features/tracingEvents/at/recommeneded.ts
@@ -4,11 +4,10 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
 
-import { logger } from 'pc-nrfconnect-shared';
+import { logger, SerialPort } from 'pc-nrfconnect-shared';
 import { TDispatch } from 'pc-nrfconnect-shared/src/state';
 
 import { RootState } from '../../../appReducer';
-import { sendMacros } from '../../../components/SidePanel/Macros';
 import { getUartSerialPort } from '../../tracing/traceSlice';
 import { documentationMap } from './index';
 
@@ -62,3 +61,195 @@ export const sendRecommendedCommand =
             }
         }
     };
+
+export const recommendedAt = [
+    'AT+CFUN=1',
+    'AT+CGSN=1',
+    'AT+CGMI',
+    'AT+CGMM',
+    'AT+CGMR',
+    'AT+CEMODE?',
+    'AT%XCBAND=?',
+    'AT+CMEE?',
+    'AT+CNEC?',
+    'AT+CGEREP?',
+    'AT+CIND=1,1,1',
+    'AT+CEREG=5',
+    'AT+CEREG?',
+    'AT+COPS=3,2',
+    'AT+COPS?',
+    'AT%XCBAND',
+    'AT+CGDCONT?',
+    'AT+CGACT?',
+    'AT%CESQ=1',
+    'AT+CESQ',
+    'AT%XSIM=1',
+    'AT%XSIM?',
+    'AT+CPIN?',
+    'AT+CPINR="SIM PIN"',
+    'AT+CIMI',
+    'AT+CNEC=24',
+    'AT+CMEE=1',
+    'AT+CEER',
+    'AT%MDMEV=1',
+    'AT%CESQ=1',
+    'AT+CGEREP=1',
+    'AT%XTIME=1',
+    'AT+CSCON=1',
+    'AT%XPOFWARN=1,30',
+    'AT%XVBATLVL=1',
+    'AT%XMONITOR',
+    'AT%CONEVAL',
+    'AT%XCONNSTAT=1',
+    'AT#XPING="www.google.com",45,5000,5,1000',
+    'AT%XCONNSTAT?',
+    'AT%HWVERSION',
+    'AT%XMODEMUUID',
+    'AT%XDATAPRFL?',
+    'AT%XSYSTEMMODE?',
+];
+
+// Todo: need to set mode in store, in order to not always check for mode
+export const sendMacros = (
+    serialPort: SerialPort,
+    commands: string[],
+    mode?: boolean
+) => {
+    if (mode) {
+        subscribe(serialPort, '', commands);
+    } else {
+        testMode(serialPort, commands);
+    }
+};
+
+const testMode = (serialPort: SerialPort, commands: string[]) => {
+    const decoder = new TextDecoder();
+    let prefix = 'at ';
+    let response = '';
+
+    const testHandler = serialPort.onData(data => {
+        response += decoder.decode(data);
+        const doCompare = response.endsWith('\r\n');
+        if (doCompare) {
+            if (response.includes('ERROR')) {
+                prefix = '';
+            }
+
+            testHandler();
+            subscribe(serialPort, prefix, commands);
+        }
+    });
+
+    serialPort.write(`${prefix} AT\r\n`);
+};
+
+const subscribe = (
+    serialPort: SerialPort,
+    prefix: string,
+    commands: string[]
+) => {
+    const decoder = new TextDecoder();
+    let commandIndex = 0;
+    let response = '';
+
+    const handler = serialPort.onData(data => {
+        response += decoder.decode(data);
+        const doCompare = response.endsWith('\r\n');
+        const doContinue =
+            (doCompare && response.includes('OK')) ||
+            response.includes('ERROR');
+        if (doContinue) {
+            commandIndex += 1;
+
+            if (commandIndex < commands.length) {
+                serialPort.write(`${prefix}${commands[commandIndex]}\r\n`);
+            } else {
+                // Cleanup when all commands have been sent.
+                handler();
+            }
+            response = '';
+        }
+    });
+
+    serialPort.write(`${prefix}${commands[commandIndex]}\r\n`);
+};
+
+const analysisSetup = [
+    // General Initial Setup
+    'AT+CFUN=4',
+    'AT+CPSMS=',
+    'AT+CGMR',
+    'AT+CNEC=24',
+    'AT+CGEREP=1',
+    'AT%MDMEV=1',
+    'AT+CEREG=5',
+    'AT%XSIM=1',
+    'AT+CSCON=1',
+    'AT%REL14FEAT=0,1,0,0,0',
+];
+
+const simCardSetup = [
+    // SimCard Setup
+    'AT+CFUN=41',
+    'AT+CNUM',
+    'AT+CIMI',
+    'AT%XICCID',
+    'AT+CPIN?',
+];
+
+const startupModem = [
+    // Modem Running
+    'AT%XSYSTEMMODE=1,0,0,0',
+    'AT+CFUN=1',
+    'AT+%CONEVAL',
+    'AT%XMONITOR',
+    'AT+CGDCONT?',
+    'AT%NBRGRSRP',
+];
+
+const checkAsRai = [
+    // Check AS-RAI
+    'AT%RAI=1',
+];
+
+const checkPSM = [
+    // Check PSM (T3412) and Active timer (T3324) values
+    // PSM and eDRX
+    'AT+CPSMS=1,"","","11000001","01011111"',
+    'AT+CEDRXS=2,4, "0000","0000"',
+];
+
+const getAvailableOperators = [
+    // Get available operators
+    'AT%COPS=?',
+];
+
+const setupNbIot = [
+    // Setup NB-IOT
+    'AT+CFUN=4',
+    'AT%RAI=0',
+    'AT%XSYSTEMMODE=0,1,0,0',
+    'AT+CFUN=1',
+    'AT+%CONEVAL',
+    'AT%XMONITOR',
+    'AT+CGDCONT?',
+    'AT%NBRGRSRP',
+];
+
+export const ltemReport = [
+    ...analysisSetup,
+    ...simCardSetup,
+    ...startupModem,
+    ...checkAsRai,
+    ...checkPSM,
+    ...getAvailableOperators,
+];
+
+export const nbIotReport = [
+    ...setupNbIot,
+    ...checkAsRai,
+    ...checkPSM,
+    ...getAvailableOperators,
+];
+
+export const fullReport = [...ltemReport, ...nbIotReport];
