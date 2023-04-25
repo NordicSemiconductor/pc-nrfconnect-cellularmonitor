de77835c7e2055deb9c8815709f7929b38ef6dce
aadnekar <aadne.karstad@gmail.com>
2023-04-25 10:44:34 +0200

    Add `+CGDCONT` to relevant commands

--- src/components/EventChart/Cards/PacketDomainNetwork.tsx
+++ src/components/EventChart/Cards/PacketDomainNetwork.tsx
@@ -20,14 +20,28 @@ export default ({
     cause,
 }: AccessPointName) => {
     const fields: DashboardCardFields = {
-        'Access Point Name': { value: apn ?? 'Unknown', commands: [] },
-        'PDN Type': { value: pdnType ?? 'Unknown', commands: [] },
+        'Access Point Name': {
+            value: apn ?? 'Unknown',
+            commands: ['AT+CGDCONT'],
+        },
+        'PDN Type': { value: pdnType ?? 'Unknown', commands: ['AT+CGDCONT'] },
         'PDN Type Raw': { value: rawPDNType ?? 'Unknown', commands: [] },
-        'IPv4 Address': { value: ipv4 ?? 'Unknown', commands: [] },
+        'IPv4 Address': { value: ipv4 ?? 'Unknown', commands: ['AT+CGDCONT'] },
         'IPv6 Address': { value: `${ipv6}` ?? 'Unknown', commands: [] },
         info: { value: info ?? 'Unknown', commands: [] },
         Connection: { value: connection ?? 'Unknown', commands: [] },
     };
+    const fieldsToDisplay = Object.keys(fields)
+        .filter(field => {
+            const value = fields[field].value;
+            return (
+                value !== 'Unknown' && value !== 'undefined' && value != null
+            );
+        })
+        .reduce((acc, field) => {
+            acc[field] = fields[field];
+            return acc;
+        }, {} as DashboardCardFields);
 
     if (cause) {
         fields['Cause Code'] = {
@@ -49,7 +63,7 @@ export default ({
                     : 'Unknown APN'
             }
             iconName="mdi-web"
-            fields={fields}
+            fields={fieldsToDisplay}
         />
     );
 };
--- src/features/tracingEvents/at/recommeneded.ts
+++ src/features/tracingEvents/at/recommeneded.ts
@@ -35,6 +35,7 @@ export const recommendedAT: recommendedAT = {
     'AT%XSYSTEMMODE': 'AT%XSYSTEMMODE?',
 
     'AT%XEMPR': 'AT%XEMPR?',
+    'AT+CGDCONT': 'AT+CGDCONT?',
 
     // Should be last command, since AT+COPS=? is slow
     'AT+COPS': ['AT+COPS?', 'AT+COPS=?'],
