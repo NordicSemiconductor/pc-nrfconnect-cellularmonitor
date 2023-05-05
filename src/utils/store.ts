e61c21218e434fd00ee2fae4b44e338766574587
Jonas <jonas.emdal@nordicsemi.no>
2023-05-05 21:25:01 +0200

    Reset trace formats with migration

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -5,7 +5,7 @@
  */
 
 import path from 'path';
-import { getAppFile, getPersistentStore as store } from 'pc-nrfconnect-shared';
+import { getAppFile, getPersistentStore } from 'pc-nrfconnect-shared';
 
 import { ALL_TRACE_FORMATS, TraceFormat } from '../features/tracing/formats';
 
@@ -23,6 +23,14 @@ const COLLAPSE_POWER_SECTION = 'collapsePowerSection';
 const COLLAPSE_CONNECTION_STATUS_SECTION = 'connectionStatusSection';
 const SHOW_STARTUP_DIALOG = 'showStartupDialog';
 
+const store = getPersistentStore<StoreSchema>({
+    migrations: {
+        '0.4.5': instance => {
+            instance.set(TRACE_FORMATS, ['raw', 'tshark']);
+        },
+    },
+});
+
 interface StoreSchema {
     [MANUAL_DB_FILE_PATH_KEY]: string | undefined;
     [WIRESHARK_EXECUTABLE_PATH_KEY]: string | null;
@@ -37,54 +45,50 @@ interface StoreSchema {
 export const autoDetectDbRootFolder = () =>
     getAppFile(path.join('resources', 'traceDB')) as string;
 
-export const getManualDbFilePath = () =>
-    store<StoreSchema>().get(MANUAL_DB_FILE_PATH_KEY);
+export const getManualDbFilePath = () => store.get(MANUAL_DB_FILE_PATH_KEY);
 export const setManualDbFilePath = (manualDbFilePath: string) =>
-    store<StoreSchema>().set(MANUAL_DB_FILE_PATH_KEY, manualDbFilePath);
-export const deleteDbFilePath = () =>
-    store<StoreSchema>().delete(MANUAL_DB_FILE_PATH_KEY);
+    store.set(MANUAL_DB_FILE_PATH_KEY, manualDbFilePath);
+export const deleteDbFilePath = () => store.delete(MANUAL_DB_FILE_PATH_KEY);
 
 export const getWiresharkPath = () =>
-    store<StoreSchema>().get(WIRESHARK_EXECUTABLE_PATH_KEY, null);
+    store.get(WIRESHARK_EXECUTABLE_PATH_KEY, null);
 export const setWiresharkPath = (wiresharkPath: string) =>
-    store<StoreSchema>().set(WIRESHARK_EXECUTABLE_PATH_KEY, wiresharkPath);
+    store.set(WIRESHARK_EXECUTABLE_PATH_KEY, wiresharkPath);
 
-export const getTsharkPath = () =>
-    store<StoreSchema>().get(TSHARK_EXECUTABLE_PATH_KEY, null);
+export const getTsharkPath = () => store.get(TSHARK_EXECUTABLE_PATH_KEY, null);
 export const setTsharkPath = (tsharkPath: string) =>
-    store<StoreSchema>().set(TSHARK_EXECUTABLE_PATH_KEY, tsharkPath);
+    store.set(TSHARK_EXECUTABLE_PATH_KEY, tsharkPath);
 
 export const getTraceFormats = () =>
-    store<StoreSchema>().get(TRACE_FORMATS, [ALL_TRACE_FORMATS[0]]);
+    store.get(TRACE_FORMATS, [ALL_TRACE_FORMATS[0]]);
 export const setTraceFormats = (traceFormats: TraceFormat[]) =>
-    store<StoreSchema>().set(TRACE_FORMATS, traceFormats);
+    store.set(TRACE_FORMATS, traceFormats);
 
-const serialPorts = () => store<StoreSchema>().get(SERIALPORTS, {});
+const serialPorts = () => store.get(SERIALPORTS, {});
 export const getSerialPort = (serialNumber: string) =>
     serialPorts()[serialNumber];
 
 export const setSerialPort = (serialNumber: string, port: string) =>
-    store<StoreSchema>().set(SERIALPORTS, {
+    store.set(SERIALPORTS, {
         ...serialPorts(),
         [serialNumber]: port,
     });
 
 export const getCollapsePowerSection = () =>
-    store<StoreSchema>().get(COLLAPSE_POWER_SECTION, false);
+    store.get(COLLAPSE_POWER_SECTION, false);
 export const setCollapsePowerSection = (collapsePowerSection: boolean) =>
-    store<StoreSchema>().set(COLLAPSE_POWER_SECTION, collapsePowerSection);
+    store.set(COLLAPSE_POWER_SECTION, collapsePowerSection);
 
 export const getCollapseConnectionStatusSection = () =>
-    store<StoreSchema>().get(COLLAPSE_CONNECTION_STATUS_SECTION, false);
+    store.get(COLLAPSE_CONNECTION_STATUS_SECTION, false);
 export const setCollapseConnectionStatusSection = (
     collapseConnectionStatusSection: boolean
 ) =>
-    store<StoreSchema>().set(
+    store.set(
         COLLAPSE_CONNECTION_STATUS_SECTION,
         collapseConnectionStatusSection
     );
 
 export const setShowStartupDialog = (showStartupDialog: boolean) =>
-    store<StoreSchema>().set(SHOW_STARTUP_DIALOG, showStartupDialog);
-export const getShowStartupDialog = () =>
-    store<StoreSchema>().get(SHOW_STARTUP_DIALOG, true);
+    store.set(SHOW_STARTUP_DIALOG, showStartupDialog);
+export const getShowStartupDialog = () => store.get(SHOW_STARTUP_DIALOG, true);
