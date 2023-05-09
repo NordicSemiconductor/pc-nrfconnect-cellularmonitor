bf05c11b8179c77f935fe301b1847390a755ed70
aadnekar <aadne.karstad@gmail.com>
2023-05-09 07:51:37 +0200

    Add toggle inside the dialog
    It's easier for the user to to understand how to turn of `Open in
    Wireshark` if the option to disable it is in the Dialog itself.
    
    Additionally, when toggling it in the Dialog, the toggle in the Side
    Menu is also visibly being toggled, so that it may educate the user.

--- src/components/InstallWiresharkDialog.tsx
+++ src/components/InstallWiresharkDialog.tsx
@@ -5,8 +5,14 @@
  */
 
 import React from 'react';
-import { InfoDialog } from 'pc-nrfconnect-shared';
+import { useDispatch, useSelector } from 'react-redux';
+import { Button, InfoDialog, Toggle } from 'pc-nrfconnect-shared';
 
+import {
+    getOpenInWiresharkSelected,
+    getTraceFormats,
+    setTraceFormats,
+} from '../features/tracing/traceSlice';
 import { WIRESHARK_DOWNLOAD_URL } from '../features/wireshark/wireshark';
 import { SelectWireshark } from './Wireshark/Wireshark';
 
@@ -17,38 +23,66 @@ interface InstallWiresharkDialog {
 const InstallWiresharkDialog = ({
     isVisible,
     setIsVisible,
-}: InstallWiresharkDialog) => (
-    <InfoDialog
-        title="Could not find Wireshark"
-        isVisible={isVisible}
-        onHide={() => setIsVisible(false)}
-    >
-        <p>
-            Could not find Wireshark on your system, you have three options to
-            proceed.
-        </p>
-        <ol>
-            <li>
-                <a
-                    rel="noreferrer"
-                    target="_blank"
-                    href={WIRESHARK_DOWNLOAD_URL}
-                >
-                    Install Wireshark
-                </a>
-            </li>
-            {process.platform === 'darwin' || process.platform === 'win32' ? (
+}: InstallWiresharkDialog) => {
+    const dispatch = useDispatch();
+    const traceFormats = useSelector(getTraceFormats);
+    const openInWiresharkSelected = useSelector(getOpenInWiresharkSelected);
+
+    const removeOpenInWireshark = () => {
+        const traceFormatsWithoutWireshark = traceFormats.filter(
+            format => format !== 'live'
+        );
+        dispatch(setTraceFormats(traceFormatsWithoutWireshark));
+    };
+
+    const addOpenInWireshark = () => {
+        dispatch(setTraceFormats([...traceFormats, 'live']));
+    };
+
+    return (
+        <InfoDialog
+            title="Could not find Wireshark"
+            isVisible={isVisible}
+            onHide={() => setIsVisible(false)}
+        >
+            <p>
+                Could not find Wireshark on your system, you have three options.
+            </p>
+            <ol>
                 <li>
-                    <SelectWireshark>
-                        Manually set wireshark path
-                    </SelectWireshark>
+                    <a
+                        rel="noreferrer"
+                        target="_blank"
+                        href={WIRESHARK_DOWNLOAD_URL}
+                    >
+                        Install Wireshark
+                    </a>
                 </li>
-            ) : (
-                <li>Add Wiershark to your PATH</li>
-            )}
-            <li> Start Trace without `Open in Wireshark`</li>
-        </ol>
-    </InfoDialog>
-);
+                {process.platform === 'darwin' ||
+                process.platform === 'win32' ? (
+                    <li>
+                        <SelectWireshark>
+                            Manually specify install path
+                        </SelectWireshark>
+                    </li>
+                ) : (
+                    <li>Add Wiershark to your PATH</li>
+                )}
+                <li>
+                    Disable
+                    <Toggle
+                        label="Open in Wireshark"
+                        isToggled={openInWiresharkSelected}
+                        onToggle={
+                            openInWiresharkSelected
+                                ? removeOpenInWireshark
+                                : addOpenInWireshark
+                        }
+                    />
+                </li>
+            </ol>
+        </InfoDialog>
+    );
+};
 
 export default InstallWiresharkDialog;
