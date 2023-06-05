a5fff86b79442477a53909dc27333a3810ae05c8
Jonas Fritzke Emdal <jonas.emdal@nordicsemi.no>
2023-06-05 09:51:27 +0200

    Default open Advanced Options

--- src/components/SidePanel/AdvancedOptions.tsx
+++ src/components/SidePanel/AdvancedOptions.tsx
@@ -17,7 +17,7 @@ export default () => {
     if (!device) return null;
 
     return (
-        <CollapsibleGroup heading="Advanced Options" defaultCollapsed>
+        <CollapsibleGroup heading="Advanced Options">
             <ProgramSampleModal />
             <SourceSelector />
         </CollapsibleGroup>
