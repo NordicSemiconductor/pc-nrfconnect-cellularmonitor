2f7bb4c4387116f0c7f669db77d874c167749bf2
aadnekar <aadne.karstad@gmail.com>
2023-06-08 11:07:03 +0200

    Update how to use

--- src/components/DocumentationSection.tsx
+++ src/components/DocumentationSection.tsx
@@ -10,18 +10,20 @@ import { DocumentationSection } from 'pc-nrfconnect-shared';
 import StartupDialog from '../features/startup/startupDialog';
 
 const DocumentationSections = [
-    <DocumentationSection
-        key="infocenter"
-        linkLabel="Go to Infocenter"
-        link="https://infocenter.nordicsemi.com/topic/ug_trace_collector/UG/trace_collector/intro.html"
-    >
-        Visit our Infocenter for more documentation about using the app.
-    </DocumentationSection>,
+    // <DocumentationSection
+    //     key="infocenter"
+    //     linkLabel="Go to Infocenter"
+    //     link="https://infocenter.nordicsemi.com/topic/ug_trace_collector/UG/trace_collector/intro.html"
+    // >
+    //     Visit our Infocenter for more documentation about using the app.
+    // </DocumentationSection>,
     <DocumentationSection
         key="credentialManager"
         linkLabel="Credential Manager"
         link="https://infocenter.nordicsemi.com/topic/ug_link_monitor/UG/link_monitor/lm_certificate_manager.html"
-    />,
+    >
+        Visit our Infocenter for more documentation about using the app.
+    </DocumentationSection>,
     <StartupDialog key="default-startup-dialog" />,
 ];
 
