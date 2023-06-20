0642cb7aa1c7eed965acf9fcac20c0443b18c778
aadnekar <aadne.karstad@gmail.com>
2023-06-20 16:27:39 +0200

    fix: CPSMS default values
    Separate default values for when PSM is disabled and enabled. `CPSMS=`
    should disable PSM (0b11100000), and reset the requested values.
    `CPSMS=0` should only disable PSM, but not reset the default values.

--- src/features/tracingEvents/at/commandProcessors/powerSavingModeSettings.ts
+++ src/features/tracingEvents/at/commandProcessors/powerSavingModeSettings.ts
@@ -22,14 +22,24 @@ const defaultT3412Extended = parsePowerSavingMode(
     '00000110',
     TAU_TYPES.SLEEP_INTERVAL
 );
+const disabledT3324 = parsePowerSavingMode('11100000', TAU_TYPES.ACTIVE_TIMER);
+const disabledT3412Extended = parsePowerSavingMode(
+    '11100000',
+    TAU_TYPES.SLEEP_INTERVAL
+);
 
 const resetToDefault = {
-    state: 'off',
     T3324: defaultT3324,
     T3412Extended: defaultT3412Extended,
 } as PowerSavingModeEntries;
 
+const resetToDisabled = {
+    T3324: disabledT3324,
+    T3412Extended: disabledT3412Extended,
+} as PowerSavingModeEntries;
+
 let setRequested: PowerSavingModeEntries;
+let storedPowerSavingMode: PowerSavingModeEntries;
 
 export const processor: Processor<'+CPSMS'> = {
     command: '+CPSMS',
@@ -41,14 +51,20 @@ export const processor: Processor<'+CPSMS'> = {
     onRequest: (packet, state) => {
         if (packet.requestType === RequestType.SET_WITH_VALUE) {
             if (packet.payload) {
-                setRequested = parsePowerSavingModePayload(packet.payload);
+                setRequested = parsePowerSavingModePayload(
+                    packet.payload,
+                    state?.powerSavingMode?.requested
+                );
             } else {
                 // Then we must have gotten the `AT+CPSMS=` command
                 // Which means we should turn PSM off and reset timers to default.
-                setRequested = resetToDefault;
+                console.log('Got the AT+CPSMS= command');
+                storedPowerSavingMode = resetToDefault;
+                setRequested = resetToDisabled;
             }
         }
         if (packet.requestType === RequestType.SET) {
+            console.log('This is just the RequestType.Set');
             setRequested = resetToDefault;
         }
         return state;
@@ -88,25 +104,40 @@ export const processor: Processor<'+CPSMS'> = {
     },
 };
 
-const parsePowerSavingModePayload = (payload: string) => {
+const parsePowerSavingModePayload = (
+    payload: string,
+    currentRequestedValues?: PowerSavingModeEntries
+) => {
     const [mode, , , T3412ExtendedBitmask, T3324Bitmask] =
         getParametersFromResponse(payload);
 
-    const result = {
-        state: mode === '1' ? 'on' : 'off',
-    } as PowerSavingModeEntries;
+    if (mode === '0') {
+        if (currentRequestedValues) {
+            storedPowerSavingMode = currentRequestedValues;
+            console.log(
+                'Will persist current requested values: ',
+                currentRequestedValues
+            );
+        }
+        return { T3324: disabledT3324, T3412Extended: disabledT3412Extended };
+    }
 
-    result.T3324 =
-        T3324Bitmask !== undefined
-            ? parsePowerSavingMode(T3324Bitmask, TAU_TYPES.ACTIVE_TIMER)
-            : defaultT3324;
-    result.T3412Extended =
-        T3412ExtendedBitmask !== undefined
-            ? parsePowerSavingMode(
-                  T3412ExtendedBitmask,
-                  TAU_TYPES.SLEEP_INTERVAL
-              )
-            : defaultT3412Extended;
+    console.log(
+        'Turning on PSM, have stored values from before:',
+        storedPowerSavingMode
+    );
 
-    return result;
+    return {
+        T3324:
+            T3324Bitmask !== undefined
+                ? parsePowerSavingMode(T3324Bitmask, TAU_TYPES.ACTIVE_TIMER)
+                : storedPowerSavingMode?.T3324 ?? defaultT3324,
+        T3412Extended:
+            T3412ExtendedBitmask !== undefined
+                ? parsePowerSavingMode(
+                      T3412ExtendedBitmask,
+                      TAU_TYPES.SLEEP_INTERVAL
+                  )
+                : storedPowerSavingMode?.T3412Extended ?? defaultT3412Extended,
+    };
 };
