8f96dc9f9eb3d265932f16f0799f9a3201f885ad
Pascal Pickel <Pascal.Pickel@nordicsemi.no>
2023-06-26 15:45:22 +0200

    style: set max items in dropdown to avoid overflow

--- src/components/SidePanel/DatabaseFileOverride.tsx
+++ src/components/SidePanel/DatabaseFileOverride.tsx
@@ -118,6 +118,7 @@ export default () => {
             items={items}
             onSelect={onSelect}
             selectedItem={selectedItem}
+            numItemsBeforeScroll={10}
         />
     );
 };
