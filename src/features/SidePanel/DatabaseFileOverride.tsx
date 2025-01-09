4bceec4f5015b79a85f51171470b7f7f75670397
Sviatoslav <kiev.devel@gmail.com>
2025-01-09 22:11:40 +0100

    Fix modem trace DB selection when device changed

--- src/features/SidePanel/DatabaseFileOverride.tsx
+++ src/features/SidePanel/DatabaseFileOverride.tsx
@@ -69,6 +69,11 @@ export default () => {
     ];
 
     useEffect(() => {
+        console.log('Device has changed');
+        setSelectedItem(autoSelectItem);
+    }, [device]);
+
+    useEffect(() => {
         getDatabases(nrfDeviceVersion)
             .then(setDatabases)
             .then(() => getRemoteDatabases(nrfDeviceVersion))
