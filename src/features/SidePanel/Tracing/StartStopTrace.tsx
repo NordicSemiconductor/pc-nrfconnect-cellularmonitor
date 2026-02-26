e0f8c9a6e8fbec3bf10a80c4f7ffb05fc5ae50a3
Sviatoslav <kiev.devel@gmail.com>
2026-02-26 17:25:28 +0100

    eslint

--- src/features/SidePanel/Tracing/StartStopTrace.tsx
+++ src/features/SidePanel/Tracing/StartStopTrace.tsx
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
 
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
     logger,
